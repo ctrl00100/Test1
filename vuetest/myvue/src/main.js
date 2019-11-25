2005d7bbcfcfb7bcc394667d32b7ada39d266a9c
Admin <k1772714@163.com>
2019-11-25 15:57:39 +0800

    跑起npm

--- vuetest/myvue/src/main.js
+++ vuetest/myvue/src/main.js
@@ -10,6 +10,20 @@ import 'element-ui/lib/theme-chalk/index.css'
 Vue.use(ElementUI)
 Vue.config.productionTip = false
 
+//请求根路径
+axios.defaults.baseURL='http://127.0.0.1:9001/user/'
+
+
+//匹配token
+axios.interceptors.request.use(config => {
+  console.log(config)
+  config.headers.Authorization = window.sessionStorage.getItem("token");
+  return config;
+});
+//
+Vue.prototype.$http=axios
+Vue.request=axios
+
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
