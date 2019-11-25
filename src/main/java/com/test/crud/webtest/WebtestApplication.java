d18c705bab8d32868d677ff4085a640824b02665
Admin <k1772714@163.com>
2019-11-25 10:43:36 +0800

    添加启动项

--- src/main/java/com/test/crud/webtest/WebtestApplication.java
+++ src/main/java/com/test/crud/webtest/WebtestApplication.java
@@ -1,13 +1,32 @@
 package com.test.crud.webtest;
 
+import com.test.crud.webtest.util.IdWorker;
+import com.test.crud.webtest.util.JwtUtil;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 public class WebtestApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(WebtestApplication.class, args);
+    }
+
+
+    @Bean
+    public IdWorker idWorkker(){
+        return new IdWorker(1, 1);
+    }
+    @Bean
+    public BCryptPasswordEncoder encoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public JwtUtil jwtUtil(){
+        return new JwtUtil();
     }
 
 }
